8eddc60c6b01156a7a5b4531a33813d4bf061cfd
Frauke Von Der Haar <fvon-der@student.42heilbronn.de>
2024-10-23 13:43:26 +0200

    DONE : NORM
    DONE : Testing
    
    TODO : check ft_split func

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,19 +6,22 @@
 /*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:33:16 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/10/23 13:22:43 by fvon-der         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:42:06 by fvon-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* MAN PAGE
-DESCRIPTION
-     The isalpha() function tests for any character for which isupper(3) or islower(3) is true. 
-	 The value of the argument must be representable as an unsigned char or the value of EOF.
-RETURN VALUES
-     The isalpha() function returns zero if the character tests false and returns non-zero if the character tests true.
-*/
+// MAN PAGE
+// DESCRIPTION
+//      The isalpha() function tests for any character for 
+// which isupper(3) or islower(3) is true. 
+// 	 The value of the argument must be representable 
+// as an unsigned char or the value of EOF.
+// RETURN VALUES
+//      The isalpha() function returns zero if the character tests false and
+// returns non-zero if the character tests true.
+
 int	ft_isalpha(int c)
 {
 	return (ft_isupper(c) || ft_islower(c));
