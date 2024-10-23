603d6e672fbe5603445e86654d15475accb9efd9
Frauke Von Der Haar <fvon-der@student.42heilbronn.de>
2024-10-23 17:04:08 +0200

    BUGHUNT
    lstmap func fixed memory issues under strict tests

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fvon-der <fvon-der@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 20:13:18 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/10/08 20:13:20 by fvon-der         ###   ########.fr       */
+/*   Updated: 2024/10/23 17:02:33 by fvon-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_obj;
+	void	*new_content;
 
 	new_list = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
 	while (lst)
 	{
-		new_obj = ft_lstnew(f(lst->content));
-		if (!new_obj)
+		new_content = f(lst->content);
+		if (new_content == NULL)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
+		}
+		new_obj = ft_lstnew(new_content);
+		if (!new_obj)
+		{
+			ft_lstclear(&new_list, del);
+			return (del(new_content), NULL);
 		}
 		ft_lstadd_back(&new_list, new_obj);
 		lst = lst->next;
